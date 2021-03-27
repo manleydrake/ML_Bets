56a6c7b09d16abe981e4df6e066d0bb2428589ce
John Hill <62448643+JHill6253@users.noreply.github.com>
2021-03-27 18:56:30 -0500

    Dependancy Values

--- ML_Bets/WebApp/Startup.cs
+++ ML_Bets/WebApp/Startup.cs
@@ -28,6 +28,7 @@ namespace WebApp
             {
                 configuration.RootPath = "ClientApp/build";
             });
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
